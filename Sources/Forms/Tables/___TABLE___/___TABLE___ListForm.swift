45cc9fac376bfce4d55a856d427947dd5ed4fe84
Eric Marchand <eric.marchand@4d.com>
2019-11-08 11:47:54 +0100

    swiftlint autocorrect

--- Sources/Forms/Tables/___TABLE___/___TABLE___ListForm.swift
+++ Sources/Forms/Tables/___TABLE___/___TABLE___ListForm.swift
@@ -115,7 +115,7 @@ class ___TABLE___ListForm: ListForm___LISTFORMTYPE___ {
         let textFieldInsideUISearchBar = searchBar.value(forKey: "searchField") as? UITextField
         textFieldInsideUISearchBar?.textColor = blueColor
         textFieldInsideUISearchBar?.font = UIFont(name: "HelveticaNeue-Thin", size: 15)
-        
+
         // SearchBar placeholder style
         let textFieldInsideUISearchBarLabel = textFieldInsideUISearchBar!.value(forKey: "placeholderLabel") as? UILabel
         textFieldInsideUISearchBarLabel?.textColor = blueColor
